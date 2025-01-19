b51a8f10421c2a0f75b15d558deed6dfba849df1
Raphaël Nussbaumer <rafnuss@gmail.com>
2025-01-19 09:07:28 +0100

    Format all scripts

--- script_list_e2L.py
+++ script_list_e2L.py
@@ -3,21 +3,21 @@ import os
 import e2L
 import json
 
-with open('auth.json') as f:
+with open("auth.json") as f:
     auth = json.load(f)
 
-session = e2L.auth(auth['username'], auth['password'])
+session = e2L.auth(auth["username"], auth["password"])
 
 
 # https://api.ebird.org/v2/ref/hotspot/PR
 # listHotspot = ['L358880','L323780','L323787','L293471','L802780','L323797','L341846','L323795','L343336','L294049','L293340','L420854','L323805','L323786','L323799','L293226','L293474','L550396','L682838','L341215','L323798','L293452','L527077','L682443','L323789','L323796','L323792','L813486','L340971']
 # listHospotName = ['Cabo Rojo NWR','Hacienda La Esperanza','Reserva Natural Laguna Cartagena','Cabo Rojo - Salt Flats','La Parguera--town and nearest environs','Refugio de Vida Silvestre de Boqueron','Reserva Natural Punta Guaniquilla','Reserva Natural de Humacao','Salinas de Cabo Rojo','Viejo San Juan','Laguna Cartegena - West End','Refugio Vida Silvestre de Boqueron','Reserva Natural Ca√±o Tiburones','Bosque Estatal de Ganica','Faro de Rincon','Punta Algarrobo','Cabo Rojo--Mangrove Flats','Palmas Altas','La Boca, Barceloneta','Charca Salobre Los Amadores','Faro de Cabo Rojo','Laguna Cartegena--Tower','Jagueyes-Jobos Bay','Cabo Rojo NWR--Cambate access','Reserva Natural Laguna Tortuguero','Bosque Estatal de Pinones','Parque Central de San Juan','Bosque Nacional El Yunque','Parque Nacional Cerro Gordo']
 
-listHotspot = ['L1801824']
-listHospotName = ['Ngulia Lodge']
+listHotspot = ["L1801824"]
+listHospotName = ["Ngulia Lodge"]
 
-lang = ['EN']
-cat = ['species']
+lang = ["EN"]
+cat = ["species"]
 byear = 1900
 eyear = 2050
 bmonth = 1
@@ -25,32 +25,47 @@ emonth = 12
 target_loc = "world"
 target_time = "life"
 
-format = 'letterpaper,margin=.3in,twocolumn'
+format = "letterpaper,margin=.3in,twocolumn"
 family = False
-spacing = '.95'
+spacing = ".95"
 condition_tableau = [
-    'Main table display only non-hybrid birds with occurence >1\\%.', " ( bird['freq']['year'] >= 0.01)"]
+    "Main table display only non-hybrid birds with occurence >1\\%.",
+    " ( bird['freq']['year'] >= 0.01)",
+]
 condition_rare = [
-    '\\footnotesize{>.1\\%}', " (bird['freq']['year'] < .00) and (bird['freq']['year'] > 0.001)"]
+    "\\footnotesize{>.1\\%}",
+    " (bird['freq']['year'] < .00) and (bird['freq']['year'] > 0.001)",
+]
 
 for i, h in enumerate(listHotspot):
     projet_name = listHospotName[i]
-    filename = projet_name.replace(' ', '_').replace("'", '').replace('.', '')
+    filename = projet_name.replace(" ", "_").replace("'", "").replace(".", "")
     code_loc = h
     info, bird_list = e2L.bird_creator(
-        session, code_loc, lang, cat, byear, eyear, bmonth, emonth)
+        session, code_loc, lang, cat, byear, eyear, bmonth, emonth
+    )
     bird_list = e2L.statusInList(
-        'target-world', bird_list, session, target_loc, target_time)
-    bird_list = e2L.statusInList(
-        'target-KE', bird_list, session, "KE", target_time)
+        "target-world", bird_list, session, target_loc, target_time
+    )
+    bird_list = e2L.statusInList("target-KE", bird_list, session, "KE", target_time)
     col = []
-    col.append(e2L.TableInput(info, 'checkbox', 1))
-    statusTable = [['target-KE', 'bold'], ['target-world', 'sym-*']]
-    col.append(e2L.TableInput(info, 'lang', 'EN', statusTable))
+    col.append(e2L.TableInput(info, "checkbox", 1))
+    statusTable = [["target-KE", "bold"], ["target-world", "sym-*"]]
+    col.append(e2L.TableInput(info, "lang", "EN", statusTable))
     # col.append(e2L.TableInput(info,'freq','year',0))
     # col.append(e2L.TableInput(info,'freq','season',0))
-    col.append(e2L.TableInput(info, 'freq', 'month', 0))
-    e2L.write_to_latex(projet_name, filename, bird_list, col,
-                       condition_tableau, condition_rare, family, format, spacing, info)
-    os.system('pdflatex -output-directory=./latex/ ./latex/' + filename + '.tex ')
+    col.append(e2L.TableInput(info, "freq", "month", 0))
+    e2L.write_to_latex(
+        projet_name,
+        filename,
+        bird_list,
+        col,
+        condition_tableau,
+        condition_rare,
+        family,
+        format,
+        spacing,
+        info,
+    )
+    os.system("pdflatex -output-directory=./latex/ ./latex/" + filename + ".tex ")
     input()
--- setup.py
+++ setup.py
@@ -4,21 +4,20 @@ from setuptools import setup
 setup(
     # the name must match the folder name 'verysimplemodule'
     name="e2L",
-    version='0.1.0',
-    license='MIT',
+    version="0.1.1",
+    license="MIT",
     author="Raphaël Nussbaumer",
     author_email="<rafnuss@gmail.com>",
-    description='Generate custom PDF checklists with eBird occurrence data.',
-    long_description='eBirdToLaTex Checklist Generator is a Python module which generates a customisable bird checklist based on a specific dataset downloaded from eBird.',
+    description="Generate custom PDF checklists with eBird occurrence data.",
+    long_description="eBirdToLaTex Checklist Generator is a Python module which generates a customisable bird checklist based on a specific dataset downloaded from eBird.",
     url="https://github.com/Zoziologie/ebird2latex",
     download_url="https://github.com/Zoziologie/ebird2latex/archive/refs/tags/v0.0.3.tar.gz",
     py_modules=["e2L"],
-
-    keywords=['python', 'eBird', 'LaTeX'],
     install_requires=["requests", "lxml"],
+    keywords=["python", "eBird", "LaTeX"],
     classifiers=[
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-    ]
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
 )
